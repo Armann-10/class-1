5c907d86993d5461509c3c2ba98264c82e301ac1
Armann-10 <145499181+Armann-10@users.noreply.github.com>
2023-09-20 01:43:19 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+echo "Creative IT";
+echo "<br>"."<br>";
+
+$username= "Arman Hossain";
+echo $username;
+echo $username;
+echo $username;
+echo $username;
+echo $username;
+echo $username;
+echo $username;
+echo $username;
+echo $username;
+
+$x= 5;
+$my_array= array(1,2,3,4,5);
+echo "<pre>";
+print_r ($my_array);
+
+$students = array ("Thoha","Adnan", "Mothakkin", "Araf", "Rihan","Aramn");
+print_r ($students);
+
+$student = array(1,2,3,4,5);
+echo "<br>"."<br>";
+echo '<pre>';
+$num = 10;
+echo $num;
+
+$num = 30;
+echo $num;
+
+
+$num = 40;
+echo $num;
+
+echo "<br>"."<br>";
+
+$firstname= "Md Arman";
+$lastname= "Hossain";
+$age =23;
+
+echo "I am ". $firstname ." ". $lastname . " and I am ". $age . " years old";
+
+
+echo "I am $firstname $lastname and I am $age years old ";
+
+echo $firstname." ".$lastname;
+
+
+echo "I am {$firstname} {$lastname} and I am {$age} years old ";
+echo "<br>"."<br>";
+define("PI",3.1416);
+echo PI;
+echo "<br>"."<br>";
+
+$nid = "212435663345";
+echo ++$nid;
